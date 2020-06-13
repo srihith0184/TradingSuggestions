529b67239a26d6add720edd99f19a799aab93157
USER <USER@DESKTOP-VEJB4IF>
2020-06-13 10:06:14 +0530

    Test

---
+++ TradingSuggestions/src/main/java/com/trading/TradingSuggestionsApplication.java
@@ -0,0 +1,13 @@
+package com.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSuggestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingSuggestionsApplication.class, args);
+	}
+
+}
